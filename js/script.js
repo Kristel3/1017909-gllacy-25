a6e4cd49c548010193d2ff9436aeb3360fb32cae
Kristel3 <50045133+Kristel3@users.noreply.github.com>
2019-07-06 12:42:46 +0400

    Задание#9

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-var link = document.querySelector(".button.button-feedback");
+var link = document.querySelector(".button-feedback");
 
 var popup = document.querySelector(".feedback");
 var close = popup.querySelector(".close");
@@ -18,35 +18,34 @@ try {
 }
 
 link.addEventListener("click", function (evt) {
-List.add("modal-show"); evt.preventDefault();
-popup.class
-link.onclick = function() {
-popup.style.display = "block";
-}
+  evt.preventDefault();
+  popup.classList.add("modal-show");
+
   if (storage) {
     name.value = storage;
-    mail.focus();
+    mail.value = storage;
+    comment.focus();
   } else {
     name.focus();
-};
+  }
+});
 
 close.addEventListener("click", function (evt) {
-evt.preventDefault();
-popup.classList.remove("modal-show");
-popup.classList.remove("modal-error");
+  evt.preventDefault();
+  popup.classList.remove("modal-show");
+  popup.classList.remove("modal-error");
 });
 
 form.addEventListener("submit", function (evt) {
   if (!name.value || !mail.value) {
     evt.preventDefault();
-    console.name("Нужно ввести имя и адрес почты");
     popup.classList.remove("modal-error");
     popup.offsetWidth = popup.offsetWidth;
     popup.classList.add("modal-error");
   } else {
     if (isStorageSupport) {
-      localStorage.setItem("mail", mail.value);
       localStorage.setItem("name", name.value);
+      localStorage.setItem("mail", mail.value);
     }
   }
 });
@@ -59,4 +58,4 @@ window.addEventListener("keydown", function (evt) {
       popup.classList.remove("modal-error");
     }
   }
-})})
+})
